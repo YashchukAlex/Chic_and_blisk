914948bb0546e42e5e549ad4244e250e38530dc9
Your_Name <oleksiiyashchuk@MacBook-Pro-Oleksii.local>
2020-07-08 04:34:35 +0300

    rename project

--- android/app/src/main/java/com/magisoftsolutions/shikiblisk/MainActivity.java
+++ android/app/src/main/java/com/magisoftsolutions/shikiblisk/MainActivity.java
@@ -1,4 +1,4 @@
-package com.chic_and_blisk;
+package com.magisoftsolutions.shikiblisk;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "Chic_and_blisk";
+    return "ShikiBlisk";
   }
 }
--- ios/ShikiBliskTests/ShikiBliskTests.m
+++ ios/ShikiBliskTests/ShikiBliskTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface Chic_and_bliskTests : XCTestCase
+@interface ShikiBliskTests : XCTestCase
 
 @end
 
-@implementation Chic_and_bliskTests
+@implementation ShikiBliskTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
